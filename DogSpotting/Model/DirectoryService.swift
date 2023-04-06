83184c155a0aca7767d597c1023325c4d24385d1
Cory Tripathy <ctripathy0@gmail.com>
2023-04-06 13:30:02 -0400

    added directory service class

---
+++ DogSpotting/Model/DirectoryService.swift
@@ -0,0 +1,44 @@
+//
+//  DirectoryService.swift
+//  DogSpotting
+//
+//  Created by Cory Tripathy on 4/6/23.
+//
+
+import Foundation
+
+/// Manages persisting your models to the phone's file directory
+class DirectoryService {
+    let encoder = JSONEncoder()
+    let decoder = JSONDecoder()
+    let directory: () throws -> URL = {
+        try FileManager.default
+            .url(for: .documentDirectory,
+                 in: .userDomainMask,
+                 appropriateFor: nil,
+                 create: false)
+    }
+    
+    func saveModelToDisk<T: Codable>(_ models: [T]) {
+        do {
+            try encoder
+                .encode(models)
+                .write(to: directory())
+        } catch {
+            debugPrint(error)
+        }
+    }
+    
+    func readModelToDisk<T:Codable>() -> [T] {
+        do {
+            let encodedModels = try Data(contentsOf: directory())
+            let decodedModels = try decoder
+                .decode([T].self, from: encodedModels)
+            return decodedModels
+        } catch {
+            debugPrint(error)
+            return []
+        }
+    }
+    static let shared = DirectoryService()
+}
