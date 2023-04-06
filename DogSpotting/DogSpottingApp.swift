d73bf2e864ed3085c8ef86af839efaf5f09fecd4
Zoe Cutler <zcutlerf@gmail.com>
2023-04-06 10:08:29 -0400

    Initial Commit

---
+++ DogSpotting/DogSpottingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogSpottingApp.swift
+//  DogSpotting
+//
+//  Created by Zoe Cutler on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DogSpottingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
