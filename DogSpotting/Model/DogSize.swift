60accbd6ddd789784a6943fbd7adeaf6f08f9cae
Zoe Cutler <zcutlerf@gmail.com>
2023-04-11 14:34:43 -0400

    Added DogSize enum stub

---
+++ DogSpotting/Model/DogSize.swift
@@ -0,0 +1,13 @@
+//
+//  DogSize.swift
+//  DogSpotting
+//
+//  Created by Zoe Cutler on 4/11/23.
+//
+
+import Foundation
+
+enum DogSize: String, CaseIterable {
+    //TODO: Add cases to represent the different sizes of dogs that exist.
+    case idk
+}
