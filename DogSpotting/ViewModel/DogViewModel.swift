7b065311624ea19382b13b03cb09383874f3ba6e
Cory Tripathy <ctripathy0@gmail.com>
2023-04-11 10:53:51 -0400

    view model stubs

--- DogSpotting/ViewModel/DogViewModel.swift
+++ DogSpotting/ViewModel/DogViewModel.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 
-@MainActor
 class DogViewModel: ObservableObject {
-//    let directoryService = DirectoryService()
+        
+    init() {
+        // code to run when a ViewModel instance is created
+    }
     
-    public func addNewDog(_ name: String, size: String) { }
+    func addNewDog(_ name: String, size: String) { }
     
+    func saveDogs() {
+        // Use the function defined in the Encodable extension to save your dogs
+    }
     
+    func loadDogs() {
+        // Use the function defined in the Decodable extension to load your dogs
+    }
 }
